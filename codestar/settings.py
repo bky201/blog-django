34fcf546d6d514077460c4540161f7eeb34bfab1
bek <bekrye@gmail.com>
2023-07-09 21:10:42 +0200

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-ALLOWED_HOSTS = ['blog-django-app.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
