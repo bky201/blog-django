a26bc57fe49bd8b9714f48c6c5e12c4d61d67f5b
bek <bekrye@gmail.com>
2023-07-12 18:03:53 +0200

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,8 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-development = os.environ.get('DEVELOPMENT', False)
-DEBUG = development
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
